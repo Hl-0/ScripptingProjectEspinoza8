fca77142e9dcb1f5701f90e615a777ebdff6be5f
Angel Samuel Espinoza <759412@student.dusd.net>
2023-11-29 15:28:44 -0800

    added a robot
    just added robots

--- RubysAdevtureEspinoza/Assets/Scripts/RubyController.cs
+++ RubysAdevtureEspinoza/Assets/Scripts/RubyController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
+using UnityEditor.ShaderGraph.Internal;
 using UnityEngine;
 using UnityEngine.Experimental.Rendering;
 using UnityEngine.UIElements;
@@ -8,39 +9,21 @@ using UnityEngine.UIElements;
 public class RubyController : MonoBehaviour
 {
     public float speed = 3.0f;
-<<<<<<< HEAD
     public int maxHealth = 5;
-    public int health { get { return currentHealth; } }
+    public float timeInvincible = 2;
     int currentHealth;
-           
-
-=======
-
-    public int maxHealth = 5;
+    public int health { get { return currentHealth; } }
 
     public GameObject projectilePrefab;
 
-    public float timeInvincible = 2;
-    public int health { get { return currentHealth; } }
-    int currentHealth;
-
     bool isInvincible;
     float invincibleTimer;
->>>>>>> d2f4d342615badfcba1b4c8a584162529d0fed83
 
     Rigidbody2D rigidbody2d;
     float horizontal;
     float vertical;
-<<<<<<< HEAD
-    // Start is called before the first frame update
-    void Start()
-    {
-        rigidbody2d = GetComponent<Rigidbody2D>();
-        currentHealth = maxHealth;
-=======
-
     Animator animator;
-    Vector2 lookdirection = new Vector2(1,0);
+    Vector2 lookDirection = new Vector2 (1, 0);
 
     // Start is called before the first frame update
     void Start()
@@ -49,7 +32,6 @@ public class RubyController : MonoBehaviour
         currentHealth = maxHealth;
         animator = GetComponent<Animator>();
         
->>>>>>> d2f4d342615badfcba1b4c8a584162529d0fed83
     }
 
     // Update is called once per frame
@@ -57,22 +39,19 @@ public class RubyController : MonoBehaviour
     {
 
         horizontal = Input.GetAxis("Horizontal");
-<<<<<<< HEAD
         vertical = Input.GetAxis("Vertical");
-    }
+    
 
-=======
-         vertical = Input.GetAxis("Vertical");
 
         Vector2 move = new Vector2(horizontal, vertical);
 
         if (!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f) )
         {
-            lookdirection.Set(move.x, move.y);
-            lookdirection.Normalize();
+            lookDirection.Set(move.x, move.y);
+            lookDirection.Normalize();
         }
-        animator.SetFloat("Look X", lookdirection.x);
-        animator.SetFloat("Look Y", lookdirection.y);
+        animator.SetFloat("Look X", lookDirection.x);
+        animator.SetFloat("Look Y", lookDirection.y);
         animator.SetFloat("Speed", move.magnitude);
 
         if (isInvincible)
@@ -90,7 +69,7 @@ public class RubyController : MonoBehaviour
     }
    
    
->>>>>>> d2f4d342615badfcba1b4c8a584162529d0fed83
+
     void FixedUpdate()
     {
 
@@ -103,12 +82,6 @@ public class RubyController : MonoBehaviour
     }
     public void ChangeHealth(int amount)
     { 
-<<<<<<< HEAD
-        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
-        Debug.Log(currentHealth + "/" + maxHealth);
-    }
-    
-=======
         if(amount < 0)
         {
             animator.SetTrigger("Hit");
@@ -121,7 +94,7 @@ public class RubyController : MonoBehaviour
             invincibleTimer = timeInvincible;
 
         }
-    currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
         Debug.Log(currentHealth + "/" + maxHealth);
     }
 
@@ -130,9 +103,9 @@ public class RubyController : MonoBehaviour
         GameObject projectileObject = Instantiate(projectilePrefab, rigidbody2d.position + Vector2.up * 0.5f, Quaternion.identity);
 
         Projectile projectile = projectileObject.GetComponent<Projectile>();
-        projectile.Launch(lookdirection, 300);
+        projectile.Launch( lookDirection, 300);
 
         animator.SetTrigger("Launch");
     }
->>>>>>> d2f4d342615badfcba1b4c8a584162529d0fed83
+
 }
